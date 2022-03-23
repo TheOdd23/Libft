35b8be9840dc3c3381ab5b1f46bb6694d29a7702
TheOdd23 <100093373+TheOdd23@users.noreply.github.com>
2022-03-23 17:06:04 -0400

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -11,7 +11,7 @@ int ft_memcmp(const void *str1, const void *str2, size_t n)
     i = 0;
     if (n <= 0)
         return (0);
-    while ((s1[i] && s2[i]) && (s1[i] == s2[i]) && i < n)
+    while ((s1[i] && s2[i]) && (s1[i] == s2[i]) && i < n - 1)
         i++;
     return (s1[i] - s2[i]);
 }
