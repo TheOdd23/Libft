e81cb0be6b8564b044adbacafd1d15c0cae66a21
TheOdd23 <100093373+TheOdd23@users.noreply.github.com>
2022-03-31 14:18:57 -0400

    Add files via upload

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,15 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anhebert <anhebert@42quebec.com            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/28 11:33:42 by anhebert          #+#    #+#             */
+/*   Updated: 2022/03/31 13:48:47 by anhebert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*Met à 0 n nombre de bytes dans la string passée en paramètre*/
+
 #include "libft.h"
 
-void    ft_bzero(void *str, size_t n)
+void	ft_bzero(void *str, size_t n)
 {
-    size_t  i;
+	size_t	i;
 
-    i = 0;
-    if (n == 0)
-        return ;
-    while (i < n)
-    {
-        ((char *)str)[i] = 0;
-        i++;
-    }
+	i = 0;
+	if (n == 0)
+		return ;
+	while (i < n)
+	{
+		((char *)str)[i] = 0;
+		i++;
+	}
 }
--- ft_calloc.c
+++ ft_calloc.c
@@ -1,12 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anhebert <anhebert@42quebec.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/28 11:34:38 by anhebert          #+#    #+#             */
+/*   Updated: 2022/03/31 13:49:55 by anhebert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*Alloue la mémoire nécessaire en mettant des 0 à tous les bytes*/
+
 #include "libft.h"
 
 void	*ft_calloc(size_t num, size_t size)
 {
-    char *new;
+	char	*new;
 
-    new = malloc(num * size);
-    if (new == NULL)
-        return (NULL);
-    ft_bzero(new, (num * size));
-    return ((void *)new);
+	new = malloc(num * size);
+	if (new == NULL)
+		return (NULL);
+	ft_bzero(new, (num * size));
+	return (new);
 }
